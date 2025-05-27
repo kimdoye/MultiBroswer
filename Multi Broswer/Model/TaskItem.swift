b057ffbaedb56c52dd4132f24ff6fe647b598abe
Doyeon <kimdoye@umich.edu>
2025-05-27 17:59:33 -0400

    Add .gitignore file

---
+++ Multi Broswer/Model/TaskItem.swift
@@ -0,0 +1,8 @@
+//
+//  TaskItem.swift
+//  Multi Broswer
+//
+//  Created by Tony Kim on 5/27/25.
+//
+
+import Foundation
---
+++ TaskManager.swift
@@ -0,0 +1,8 @@
+//
+//  TaskManager.swift
+//  Multi Broswer
+//
+//  Created by Tony Kim on 5/27/25.
+//
+
+import Foundation
